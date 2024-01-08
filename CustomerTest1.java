4ed66cf3b7869285a8a48b120126526177d821ff
Narendra Ragav Venkatesan <147092133+rockerspace@users.noreply.github.com>
2024-01-08 12:13:25 -0600

    Add files via upload

---
+++ CustomerTest1.java
@@ -0,0 +1,27 @@
+package Day4classesandobjects.test;
+
+import Day4classesandobjects.Customer;
+
+public class CustomerTest1 {
+
+	public static void main(String[] args) {
+		Customer c=new Customer();
+        c.insertCustomer();
+        
+        Customer c1=new Customer();
+        c1.insertCustomer();
+        
+        Customer c2=new Customer();
+        c2.insertCustomer();
+        
+        
+        c.showCustomer();
+        c1.showCustomer();
+        c2.showCustomer();
+	}
+
+
+
+	}
+
+
